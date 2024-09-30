34f7962dd589172bf7c3ce8f9636e5cd2d57d30b
Phil Peeters <peeters_phil50@hotmail.com>
2024-09-30 10:14:50 +0200

    structure

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,35 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">John Doe</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/">Accueil</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/services">Services</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/portfolio">Portfolio</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/blog">Blog</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/contact">Contact</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/legal">Mentions légales</Link>
-          </li>
-        </ul>
+    <nav className="navbar navbar-expand-lg navbar-custom">
+      <div className="container"> {/* Ajout de la classe container ici */}
+        <a className="navbar-brand font-bold" href="/">Philippe Peeters</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink 
+                className="nav-link text-uppercase" 
+                exact 
+                to="/" 
+                activeClassName="active" 
+              >
+                Accueil
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link text-uppercase" to="/services" activeClassName="active">Services</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link text-uppercase" to="/portfolio" activeClassName="active">Portfolio</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link text-uppercase" to="/blog" activeClassName="active">Blog</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link text-uppercase" to="/contact" activeClassName="active">Contact</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link text-uppercase" to="/legal" activeClassName="active">Mentions légales</NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,83 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './heroSection.css'; // Créer un fichier CSS pour styliser la section hero
 
-const home = () => {
+const Home = () => {
   return (
     <div>
-      <h1>Accueil</h1>
-      {/* Contenu de la page d'accueil */}
+      {/* Section Hero */}
+      <section className="hero-section d-flex justify-content-center align-items-center">
+        <div className="text-center">
+        <h1 className="hero-title"> Bonjour, je suis Philippe Peeters </h1>
+          <h2 className="hero-subtitle">Développeur web full stack</h2>
+          <Link to="/about" className="btn btn-primary mt-3">En savoir plus</Link>
+        </div>
+      </section>
+
+      {/* Section À propos */}
+      <section className="about-section container mt-5">
+        <div className="row">
+          {/* Partie gauche : A propos */}
+          <div className="col-md-6">
+            <h3>À propos</h3>
+            <p>Passionné par l’informatique et les nouvelles technologies, j’ai suivi une formation d’<strong>intégrateur-développeur web</strong> au CEF Au cours de cette formation j’ai pu acquérir des bases solides pour travailler dans le domaine du <strong>développement web</strong>.</p>               
+            <p>Basé dans la région Bordelaise à Saint Seurin de Bourg. Je souhaite créer un cabinet de développeurs créateurs de sites web et web mobiles.  Pour ce faire j’ai besoin de consolider ma formation de <strong>développeur web full stack</strong>. </p>
+            <p>J’accorde une attention particulière à la qualité du code que j’écris et je respecte les bonnes pratiques du web.</p>
+          </div>
+    
+          {/* Partie droite : Image */}
+          <div className="col-md-6">
+            <img src="/img/john-doe-about.jpg" alt="Philippe Peeters" className="img-fluid" />
+          
+          {/* Barre de compétences */}
+          <div className="skills">
+              <h5>Mes compétences</h5>
+        
+              <div className="skill">
+                <span>HTML5 90% </span>
+                <div className="progress">
+                  <div className="progress-bar html-skill" style={{ width: '90%' }}></div>
+                </div>
+              </div>
+              
+              <div className="skill">
+                <span>CSS3 80% </span>
+                <div className="progress">
+                  <div className="progress-bar css-skill" style={{ width: '80%' }}></div>
+                </div>
+              </div>
+                                  
+              <div className="skill"> 
+                <span>JavaScript 70%</span>
+                <div className="progress">
+                  <div className="progress-bar java-script-skill" style={{ width: '70%' }}></div>
+                </div>
+              </div>
+              
+              <div className="skill">
+                <span>PHP 60% </span>
+                <div className="progress">
+                  <div className="progress-bar php-skill" style={{ width: '60%' }}></div>
+                </div>
+              </div>            
+              
+              <div className="skill">
+                <span>REACT 50% </span>
+                <div className="progress">
+                  <div className="progress-bar react-skill" style={{ width: '50%' }}></div>
+                </div>
+              </div>
+       
+            </div>
+
+
+
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
 
-export default home;
+export default Home;
